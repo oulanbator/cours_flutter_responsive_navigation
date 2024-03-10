c58b2d40efdb744349222d64fc3b81af80a623d2
Victor Matheron <victor.matheron@gmail.com>
2024-03-10 13:35:00 +0100

    Implements LayoutBuilder structure

--- lib/main_navigation.dart
+++ lib/main_navigation.dart
@@ -10,6 +10,32 @@ class MainNavigation extends StatefulWidget {
 class _MainNavigationState extends State<MainNavigation> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < 450) {
+          print("Small screen");
+          return _smallScreenLayout(context);
+        } else {
+          print("Large screen");
+          return _largeScreenLayout(context);
+        }
+      },
+    );
+  }
+
+  _smallScreenLayout(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Small Screen"),
+      ),
+    );
+  }
+
+  _largeScreenLayout(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Large Screen"),
+      ),
+    );
   }
 }
